9fd2bf89d1ee7538379982db9c9bcbee2582bc3c
akbarhps <akbarputrasrg@gmail.com>
2021-01-27 18:40:44 +0700

    📦 Implement checkLocationPermission

--- app/src/main/java/com/charuniverse/mycoordinate/MainActivity.kt
+++ app/src/main/java/com/charuniverse/mycoordinate/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.charuniverse.mycoordinate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)
+            .get(MainViewModel::class.java)
+            .also {
+                it.initViewModel(this)
+                it.checkLocationPermission()
+            }
     }
 }
--- app/src/main/java/com/charuniverse/mycoordinate/MainViewModel.kt
+++ app/src/main/java/com/charuniverse/mycoordinate/MainViewModel.kt
@@ -1,11 +1,17 @@
 package com.charuniverse.mycoordinate
 
+import android.Manifest
+import android.app.Activity
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.charuniverse.mycoordinate.utils.Constants
+import pub.devrel.easypermissions.EasyPermissions
 
 class MainViewModel : ViewModel() {
+
+    private lateinit var activity: Activity
 
     private val _mainUiState = MutableLiveData<MainUiState>()
     val mainUiState: LiveData<MainUiState> = _mainUiState
@@ -18,6 +24,33 @@ class MainViewModel : ViewModel() {
             Constants.ERROR_STATE       -> MainUiState.Error(errorMessage)
             else                        -> MainUiState.Idle
         }
+    }
+
+    fun initViewModel(activity: Activity) {
+        this.activity = activity
+    }
+
+    fun checkLocationPermission() {
+        if (!hasLocationPermission()) {
+            requestLocationPermission()
+        }
+    }
+
+    private fun hasLocationPermission(): Boolean =
+        EasyPermissions.hasPermissions(
+            activity.applicationContext,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+        )
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            activity, arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            ),
+            Constants.LOCATION_PERMISSION_REQUEST_CODE
+        )
     }
 
     abstract class MainUiState {
