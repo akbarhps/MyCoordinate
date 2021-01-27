a43d1b74755103fc380130da703630dccdc5474d
akbarhps <akbarputrasrg@gmail.com>
2021-01-27 18:35:07 +0700

    📦 Create setUiState function and state constants

--- app/src/main/java/com/charuniverse/mycoordinate/MainViewModel.kt
+++ app/src/main/java/com/charuniverse/mycoordinate/MainViewModel.kt
@@ -3,16 +3,27 @@ package com.charuniverse.mycoordinate
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.charuniverse.mycoordinate.utils.Constants
 
 class MainViewModel : ViewModel() {
 
     private val _mainUiState = MutableLiveData<MainUiState>()
     val mainUiState: LiveData<MainUiState> = _mainUiState
 
+    private fun setUiState(state: String, errorMessage: String = "") {
+        _mainUiState.value = when (state) {
+            Constants.IDLE_STATE        -> MainUiState.Idle
+            Constants.LOADING_STATE     -> MainUiState.Loading
+            Constants.SUCCESS_STATE     -> MainUiState.Success
+            Constants.ERROR_STATE       -> MainUiState.Error(errorMessage)
+            else                        -> MainUiState.Idle
+        }
+    }
+
     abstract class MainUiState {
-        object Idle : MainUiState()
-        object Loading : MainUiState()
-        object Success : MainUiState()
-        data class Error(val message: String) : MainUiState()
+        object Idle                             : MainUiState()
+        object Loading                          : MainUiState()
+        object Success                          : MainUiState()
+        data class Error(val message: String)   : MainUiState()
     }
 }
---
+++ app/src/main/java/com/charuniverse/mycoordinate/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.charuniverse.mycoordinate.utils
+
+object Constants {
+    const val IDLE_STATE = "UI_IDLE_STATE"
+    const val LOADING_STATE = "UI_LOADING_STATE"
+    const val SUCCESS_STATE = "UI_SUCCESS_STATE"
+    const val ERROR_STATE = "UI_ERROR_STATE"
+}
