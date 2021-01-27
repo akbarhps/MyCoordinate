d57f62288622052c8f1fb5654b5171ae673a5ac7
akbarhps <akbarputrasrg@gmail.com>
2021-01-27 19:50:57 +0700

    📦 Implement enable location feature

--- app/src/main/java/com/charuniverse/mycoordinate/MainViewModel.kt
+++ app/src/main/java/com/charuniverse/mycoordinate/MainViewModel.kt
@@ -2,11 +2,22 @@ package com.charuniverse.mycoordinate
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
+import android.content.IntentSender
+import android.location.LocationManager
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.charuniverse.mycoordinate.utils.Constants
+import com.charuniverse.mycoordinate.utils.Constants.MAIN_VIEW_MODEL_TAG
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsStatusCodes
 import pub.devrel.easypermissions.EasyPermissions
 
 class MainViewModel : ViewModel() {
@@ -33,6 +44,11 @@ class MainViewModel : ViewModel() {
     fun checkLocationPermission() {
         if (!hasLocationPermission()) {
             requestLocationPermission()
+            return
+        }
+        if (!hasLocationEnable()) {
+            requestEnableLocation()
+            return
         }
     }
 
@@ -51,6 +67,40 @@ class MainViewModel : ViewModel() {
             ),
             Constants.LOCATION_PERMISSION_REQUEST_CODE
         )
+    }
+
+    private fun hasLocationEnable(): Boolean {
+        val locationManager = activity.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun requestEnableLocation() {
+        val locationRequest = LocationRequest.create()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        val result = LocationServices.getSettingsClient(activity)
+            .checkLocationSettings(builder.build())
+
+        result.addOnCompleteListener { task ->
+            try {
+                task.getResult(ApiException::class.java)
+            } catch (exception: ApiException) {
+                when (exception.statusCode) {
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> {
+                        try {
+                            val resolvable = exception as ResolvableApiException
+                            resolvable.startResolutionForResult(
+                                activity, LocationRequest.PRIORITY_HIGH_ACCURACY
+                            )
+                        } catch (e: IntentSender.SendIntentException) {
+                            Log.e(MAIN_VIEW_MODEL_TAG, "requestEnableLocation: ${e.message}", e)
+                        }
+                    }
+                }
+            }
+        }
     }
 
     abstract class MainUiState {
--- app/src/main/java/com/charuniverse/mycoordinate/utils/Constants.kt
+++ app/src/main/java/com/charuniverse/mycoordinate/utils/Constants.kt
@@ -1,10 +1,12 @@
 package com.charuniverse.mycoordinate.utils
 
 object Constants {
-    const val IDLE_STATE        = "UI_IDLE_STATE"
-    const val LOADING_STATE     = "UI_LOADING_STATE"
-    const val SUCCESS_STATE     = "UI_SUCCESS_STATE"
-    const val ERROR_STATE       = "UI_ERROR_STATE"
+    const val MAIN_VIEW_MODEL_TAG   = "MAIN_VIEW_MODEL"
+
+    const val IDLE_STATE            = "UI_IDLE_STATE"
+    const val LOADING_STATE         = "UI_LOADING_STATE"
+    const val SUCCESS_STATE         = "UI_SUCCESS_STATE"
+    const val ERROR_STATE           = "UI_ERROR_STATE"
 
     const val LOCATION_PERMISSION_REQUEST_CODE = 1
 }
